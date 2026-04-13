7c393223359e4615187a895893e51d62a1c07219
hoangphat53 <hoangphat5393@gmail.com>
2026-04-13 13:50:53 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -618,7 +618,7 @@
         $(document).ready(function() {
             const aiWorkspaceKey = 'ai_workspace_active';
             const container = $('#ai-chatbot-container');
-            const widgetScriptUrl = 'https://ai.craveva.com/api/v1/agents/69ccc35e7d0ece6ff702487b/widget.js';
+            const widgetScriptUrl = 'https://ai.craveva.com/api/v1/agents/69d4bdab3ab4301603dfc69a/widget.js';
             let isScriptLoaded = false;
 
             // Function to load the widget script
